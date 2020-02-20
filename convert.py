9d02580c40a861885c5e624b2846d026f175839d
bdjbray <49975301+bdjbray@users.noreply.github.com>
2020-02-19 19:20:57 -0500

    Add files via upload

--- convert.py
+++ convert.py
@@ -1,21 +1,36 @@
-def convert(input):
-    ints = (1000, 500, 100, 50, 10,5, 1)
-    nums = ('M', 'D', 'C', 'L','X','V','I')
-    result = []
-    for i in range(len(ints)):
-        count = int(input / ints[i])
-        result.append(nums[i] * count)
-        input -= ints[i] * count
-    return ''.join(result)
+def convert_to_roman(n):
+    if not isinstance(n, int):
+        return 'Please enter number'
+    if n < 1 or n > 3999:
+        return 'number out of range (must be between 1 and 4000)'
+        
+    thedict = {1:'I', 5:'V', 10:'X', 50:'L', 100:'C', 500:'D', 1000:'M'}
+    
+    if n in thedict:
+        return thedict[n]
+        
+    length = len(str(n))
+    roman_num = ''
+    for i in str(n):
+        if length == 0:
+            break
+        dig_num = pow(10, length - 1)
+        if int(i) in thedict:
+            roman_num += thedict[dig_num * int(i)]
+        elif int(i) < 4:
+            roman_num += thedict[dig_num] * int(i)
+        elif (int(i) == 4) or (int(i) == 9):
+            roman_num += thedict[dig_num] + thedict[dig_num * (int(i) + 1)]
+        elif int(i) > 5:
+            roman_num += thedict[dig_num * 5] + (thedict[dig_num] * (int(i) - 5))
+        length -= 1
+    
+    return roman_num
+    
+    
+
+    
 
 
 
 
-
-
-
-res=convert(2002)
-print("the result is:",res)
-
-#l        I   V   X   L   C   D   M
-#Value   1   5   10  50  100 500 1,000
