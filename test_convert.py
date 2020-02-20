1f467b1eedf9d8d0f6f827d1ec42cb7799ab9bd1
bdjbray <49975301+bdjbray@users.noreply.github.com>
2020-02-19 19:31:14 -0500

    Add files via upload

--- test_convert.py
+++ test_convert.py
@@ -4,11 +4,11 @@ def test_valid():
 	assert cvt.convert_to_roman(1) == 'I';
 	assert cvt.convert_to_roman(3) == 'III';
 	assert cvt.convert_to_roman(10) == 'X';
-	assert cvt.convert_to_roman(34) == 'XXXIV', '34'
+	assert cvt.convert_to_roman(34) == 'XXXIV';
 	assert cvt.convert_to_roman(62) == 'LXII';
 	assert cvt.convert_to_roman(110) == 'CX';
-    assert cvt.convert_to_roman(156) == 'CLVI', '156'
+    assert cvt.convert_to_roman(156) == 'CLVI';
     assert cvt.convert_to_roman(185) == 'CLXXXV';
-    assert cvt.convert_to_roman(675) == 'DCLXXV', '675'
+    assert cvt.convert_to_roman(675) == 'DCLXXV';
     assert cvt.convert_to_roman(2002) == 'MMII';
-    assert cvt.convert_to_roman(3999) == 'MMMCMXCIX', '3999'
+    assert cvt.convert_to_roman(3999) == 'MMMCMXCIX';
